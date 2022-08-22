c395a2010b25ab44389ec4e8bb7729a4ce91ec5c
Alex Paltinean <alex.paltinean@appointfix.com>
2022-08-22 21:18:22 +0300

    third commit

--- src/curs5/Homework4Confidential.java
+++ src/curs5/Homework4Confidential.java
@@ -40,6 +40,7 @@ public class Homework4Confidential {
     public static String[] words(String text) {
         return text.split(" +");
         // some change
+        // another change
     }
 
     public static String confidential(String text, String[] sensitiveWords) {
